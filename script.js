c6757d81ebba7cc5ca1bc1304c2c8430202b5bdb
unknown <harshkumargupta630@gmail.com>
2025-04-13 01:06:23 +0530

    fixed few bugs

--- script.js
+++ script.js
@@ -42,6 +42,15 @@ app.get('/edit/:filename', function(req, res) {
   );
 });
 
+app.post("/delete/:filename", function (req, res) {
+  const filename = req.params.filename;
+  fs.unlink(`./file/${filename}`, function (err) {
+    if (err) return res.send("Error deleting file: " + err);
+    res.redirect("/");
+  });
+}
+);
+
 
 app.post("/edit/:filename", function (req, res) {
   const previousName = req.body.previous;
@@ -60,5 +69,32 @@ app.post("/edit/:filename", function (req, res) {
   });
 });
 
+
+app.get("/show/:filename", function (req, res) {
+  const filename = req.params.filename;
+  
+  // Add check for file existence
+  if (!fs.existsSync(`./file/${filename}`)) {
+    console.error('File not found:', filename);
+    return res.render("show", { 
+      filename: null, 
+      filedata: null 
+    });
+  }
+
+  fs.readFile(`./file/${filename}`, "utf-8", function (err, data) {
+    if (err) {
+      console.error('Error reading file:', err);
+      return res.render("show", { 
+        filename: null, 
+        filedata: null 
+      });
+    }
+    res.render("show", { 
+      filename: filename,
+      filedata: data 
+    });
+  });
+});
 
 app.listen(3000);
